bacf244067c466dce4c61b25b445bed197dbb55a
mraheeldev <m.raheeldev@gmail.com>
2022-07-05 12:15:31 +0500

    under_Dev

--- app/src/main/java/com/example/googleadsadvancenew/Util.kt
+++ app/src/main/java/com/example/googleadsadvancenew/Util.kt
@@ -23,20 +23,4 @@ object Util {
             e.printStackTrace()
         }
     }
-
-    var firsRun: Boolean = true
-
-    @JvmStatic
-    fun getScreenWidth(activity: Activity): Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val windowMetrics = activity.windowManager.currentWindowMetrics
-            val insets: Insets = windowMetrics.windowInsets
-                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
-            windowMetrics.bounds.width() - insets.left - insets.right
-        } else {
-            val displayMetrics = DisplayMetrics()
-            activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
-            displayMetrics.widthPixels
-        }
-    }
 }
